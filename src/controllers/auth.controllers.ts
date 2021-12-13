d5c76c7f5c835731f508d3a76a47cd73dc92b756
RealNarukyn <alexmusu99@gmail.com>
2021-12-13 01:47:47 +0100

    pulir auth endpoint

--- src/controllers/auth.controllers.ts
+++ src/controllers/auth.controllers.ts
@@ -1,10 +1,9 @@
 import { Request, Response } from "express";
 import { HydratedDocument } from "mongoose";
+import * as bcryptjs from "bcryptjs";
 
 import { IUser } from "src/interfaces/user";
 import { UserModel } from "../models/user";
-
-import { hashPassword } from "../utils/utils";
 
 export class AuthController {
   static login = async (request: Request, response: Response) => {
@@ -26,11 +25,16 @@ export class AuthController {
     if (!user) {
       return response.json({ statusCode: 404, msg: "User Not Found..." });
     }
-    if (user.password !== password) {
+
+    const validPassword = await bcryptjs.compare(password, user.password);
+    if (!validPassword) {
       return response.json({ statusCode: 503, msg: "Incorrect Password" });
     }
 
-    return response.json({ statusCode: 200, data: user });
+    return response.json({
+      statusCode: 200,
+      data: { userID: user.userID, username: user.username },
+    });
   };
 
   static register = async (request: Request, response: Response) => {
@@ -48,8 +52,13 @@ export class AuthController {
       });
     }
 
+    const user: IUser = await UserModel.findOne({ username });
+    if (user) {
+      return response.json({ statusCode: 404, msg: "User already exists..." });
+    }
+
     // -- Hash the password
-    const h_password = await hashPassword(password.trim());
+    const h_password = await bcryptjs.hash(password, 10);
 
     // -- Get the las userID stored to make it "auto-incremental"
     const lastUser: IUser = await UserModel.findOne(
--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,18 +1,3 @@
-import * as bcrypt from "bcryptjs";
-
-export const hashPassword = async (password: string) => {
-  const saltRounds = 10;
-
-  const hashedPassword = await new Promise((resolve, reject) => {
-    bcrypt.hash(password, saltRounds, function (err, hash) {
-      if (err) reject(err);
-      resolve(hash);
-    });
-  });
-
-  return hashedPassword;
-};
-
 export const getUniqueID = (): string => {
   // Math.random should be unique because of its seeding algorithm.
   // Convert it to base 36 (numbers + letters), and grab the first 9 characters after the decimal.
